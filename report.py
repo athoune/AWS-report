5308f0101190ad3b58a0a0b4345e937b42789b6f
Mathieu Lecarme <mathieu@garambrogne.net>
2011-01-17 18:25:26 +0100

    sum

--- report.py
+++ report.py
@@ -3,9 +3,9 @@ import datetime
 
 class Report:
 	def __init__(self, f):
-		self.reader = csv.reader(open(f), delimiter=',')
+		self.f = f
 	def __iter__(self):
-		for line in self.reader:
+		for line in csv.reader(open(self.f), delimiter=','):
 			yield {
 				'service' : line[0],
 				'action': line[1:3],
@@ -13,7 +13,16 @@ class Report:
 				'end'   : line[4],
 				'value' : line[5]
 				}
+	def sum(self, action, metric):
+		total = 0.0
+		for line in self.__iter__():
+			if line['action'] == [action, metric]:
+				total += float(line['value'])
+		return total
 
 r = Report('report.csv')
-for line in r:
-	print line
+#for line in r:
+#	print line
+print
+print 'requests', r.sum('SelectGet', 'Requests')
+print 'box usage', r.sum('SelectGet', 'BoxUsage')
