3723a088ecb6f92f5dfb750089bff95e4e769e2c
Mathieu Lecarme <mathieu@garambrogne.net>
2011-01-17 21:57:02 +0100

    datetime and float

--- report.py
+++ report.py
@@ -1,28 +1,31 @@
 import csv
-import datetime
+from datetime import datetime
 
 class Report:
 	def __init__(self, f):
 		self.f = f
 	def __iter__(self):
-		for line in csv.reader(open(self.f), delimiter=','):
+		r = csv.reader(open(self.f), delimiter=',')
+		r.next()
+		for line in r:
+			print line
 			yield {
 				'service' : line[0],
 				'action': line[1:3],
-				'start' : line[3],
-				'end'   : line[4],
-				'value' : line[5]
+				'start' : datetime.strptime(line[3], '%m/%d/%y %H:%M:%S'),
+				'end'   : datetime.strptime(line[4], '%m/%d/%y %H:%M:%S'),
+				'value' : float(line[5])
 				}
 	def sum(self, action, metric):
 		total = 0.0
 		for line in self.__iter__():
 			if line['action'] == [action, metric]:
-				total += float(line['value'])
+				total += line['value']
 		return total
 
 r = Report('report.csv')
-#for line in r:
-#	print line
-print
-print 'requests', r.sum('SelectGet', 'Requests')
-print 'box usage', r.sum('SelectGet', 'BoxUsage')
+for line in r:
+	print line
+#print
+#print 'requests', r.sum('SelectGet', 'Requests')
+#print 'box usage', r.sum('SelectGet', 'BoxUsage')
